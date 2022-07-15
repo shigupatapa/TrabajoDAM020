208f2b36ac1cf89b1546f320c4bdbf0cc6072b9f
Franco Pizarro <bf.18.1342@gmail.com>
2022-07-14 21:18:46 -0400

    ajustes menores

--- cliente_entregable/lib/pages/noticias/page_addnoticias.dart
+++ cliente_entregable/lib/pages/noticias/page_addnoticias.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../../provider/noticias_service.dart';
@@ -21,37 +23,77 @@ class _PageAddNoticiaState extends State<PageAddNoticia> {
     return Scaffold(
         appBar: AppBar(
           title: Text('Agregar noticia'),
+          centerTitle: true,
+          backgroundColor: Colors.black87,
         ),
         body: Form(
             child: Padding(
-          padding: EdgeInsets.all(12),
-          child: ListView(children: [
-            TextFormField(
-              controller: tituloCtrl,
-              decoration: InputDecoration(labelText: 'Titulo'),
-            ),
-            Divider(),
-            TextFormField(
-              controller: contenidoCtrl,
-              decoration: InputDecoration(labelText: 'Contenido'),
-              maxLines: 3,
-            ),
-            Divider(),
+                padding: EdgeInsets.all(12),
+                child: Container(
+                    // height: double.infinity,
+                    // width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          "https://as2.ftcdn.net/v2/jpg/03/04/35/15/1000_F_304351519_t2XoCRj1J4yYQ3DlhyJTjzBsJQQpZ6mI.jpg",
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
+                        child: Theme(
+                            data: Theme.of(context).copyWith(
+                              colorScheme:
+                                  ColorScheme.light(primary: Colors.black87),
+                            ),
+                            child: Container(
+                              padding: EdgeInsets.all(10),
+                              child: ListView(children: [
+                                TextFormField(
+                                  controller: tituloCtrl,
+                                  decoration: InputDecoration(
+                                    labelText: 'Titulo',
+                                    fillColor: Colors.white.withOpacity(0.9),
+                                    filled: true,
+                                    contentPadding: EdgeInsets.all(15),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                  ),
+                                ),
+                                Divider(),
+                                TextFormField(
+                                  controller: contenidoCtrl,
+                                  decoration: InputDecoration(
+                                    labelText: 'Contenido',
+                                    fillColor: Colors.white.withOpacity(0.9),
+                                    filled: true,
+                                    contentPadding: EdgeInsets.all(15),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                  ),
+                                  maxLines: 10,
+                                ),
+                                Divider(),
 
-            /// la wea de la fecha
-            Divider(),
-            Container(
-              child: ElevatedButton(
-                child: Text('Agregar noticia'),
-                onPressed: () {
-                  String titulo = tituloCtrl.text.trim();
-                  String contenido = contenidoCtrl.text.trim();
-                  FirestoreService().noticiaAgregar(titulo, contenido, fecha);
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-          ]),
-        )));
+                                /// la wea de la fecha
+                                //Divider(),
+                                Container(
+                                  child: ElevatedButton(
+                                    child: Text('Agregar noticia'),
+                                    onPressed: () {
+                                      String titulo = tituloCtrl.text.trim();
+                                      String contenido =
+                                          contenidoCtrl.text.trim();
+                                      FirestoreService().noticiaAgregar(
+                                          titulo, contenido, fecha);
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ),
+                              ]),
+                            )))))));
   }
 }
--- cliente_entregable/lib/pages/noticias/page_editnoticias.dart
+++ cliente_entregable/lib/pages/noticias/page_editnoticias.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -23,52 +25,94 @@ class _PageEditNoticiaState extends State<PageEditNoticia> {
     return Scaffold(
         appBar: AppBar(
           title: Text('Editar Noticias'),
+          centerTitle: true,
+          backgroundColor: Colors.black87,
         ),
-        body: Form(
-          child: Padding(
+        body: Padding(
             padding: EdgeInsets.all(12),
-            child: FutureBuilder(
-                future: FirestoreService().getNoticia(widget.id),
-                builder: (context, AsyncSnapshot snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  var data = snapshot.data;
-                  tituloCtrl.text = data['titulo'];
-                  contenidoCtrl.text = data['contenido'];
-
-                  return ListView(children: [
-                    TextFormField(
-                      controller: tituloCtrl,
-                      decoration: InputDecoration(labelText: 'Titulo'),
+            child: Container(
+                // height: double.infinity,
+                // width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://as2.ftcdn.net/v2/jpg/03/04/35/15/1000_F_304351519_t2XoCRj1J4yYQ3DlhyJTjzBsJQQpZ6mI.jpg",
                     ),
-                    Divider(),
-                    TextFormField(
-                      controller: contenidoCtrl,
-                      decoration: InputDecoration(labelText: 'Contenido'),
-                      maxLines: 3,
-                    ),
-                    Divider(),
-
-                    /// la wea de la fecha
-                    Divider(),
-                    Container(
-                      child: ElevatedButton(
-                        child: Text('Editar'),
-                        onPressed: () {
-                          String titulo = tituloCtrl.text.trim();
-                          String contenido = contenidoCtrl.text.trim();
-                          FirestoreService().noticiaEditar(
-                              widget.id, titulo, contenido, fecha);
-                          Navigator.pop(context);
-                        },
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
+                    child: Theme(
+                      data: Theme.of(context).copyWith(
+                        colorScheme: ColorScheme.light(primary: Colors.black87),
                       ),
-                    )
-                  ]);
-                }),
-          ),
-        ));
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: FutureBuilder(
+                            future: FirestoreService().getNoticia(widget.id),
+                            builder: (context, AsyncSnapshot snapshot) {
+                              if (!snapshot.hasData) {
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              }
+                              var data = snapshot.data;
+                              tituloCtrl.text = data['titulo'];
+                              contenidoCtrl.text = data['contenido'];
+
+                              return Form(
+                                child: ListView(children: [
+                                  TextFormField(
+                                    controller: tituloCtrl,
+                                    decoration: InputDecoration(
+                                      labelText: 'Titulo',
+                                      fillColor: Colors.white.withOpacity(0.9),
+                                      filled: true,
+                                      contentPadding: EdgeInsets.all(15),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                    ),
+                                  ),
+                                  Divider(),
+                                  TextFormField(
+                                    controller: contenidoCtrl,
+                                    decoration: InputDecoration(
+                                      labelText: 'Contenido',
+                                      fillColor: Colors.white.withOpacity(0.9),
+                                      filled: true,
+                                      contentPadding: EdgeInsets.all(15),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                    ),
+                                    maxLines: 10,
+                                  ),
+                                  Divider(),
+
+                                  /// la wea de la fecha
+                                  //Divider(),
+                                  Container(
+                                    child: ElevatedButton(
+                                      child: Text('Editar'),
+                                      onPressed: () {
+                                        String titulo = tituloCtrl.text.trim();
+                                        String contenido =
+                                            contenidoCtrl.text.trim();
+                                        FirestoreService().noticiaEditar(
+                                            widget.id,
+                                            titulo,
+                                            contenido,
+                                            fecha);
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  )
+                                ]),
+                              );
+                            }),
+                      ),
+                    )))));
   }
 }
